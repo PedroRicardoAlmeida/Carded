d446a8f3d7322a6c862a81e6afa230e921df488e
Pedro Ricardo <ordepricardo13@gmail.com>
2020-09-16 11:53:19 -0300

    Inseri variáveis
    Inseri variáveis para facilitar a substituição de nomes e links

---
+++ Main.js
@@ -0,0 +1,19 @@
+/* Variáveis para substituir título do link*/
+var instagram = document.querySelector("#instagram")
+var facebook = document.querySelector("#facebook")
+var whatsapp = document.querySelector("#whatsapp")
+var site = document.querySelector("#site")
+var ifood = document.querySelector("#ifood")
+var linkedin = document.querySelector("#linkedin")
+var maps = document.querySelector("#maps")
+var telefone = document.querySelector("#telefone")
+
+/* Links para interação */
+var instagramLink = document.querySelector("#instagramLink")
+var facebookLink = document.querySelector("#facebookLink")
+var whatsappLink = document.querySelector("#whatsappLink")
+var siteLink = document.querySelector("#siteLink")
+var ifoodLink = document.querySelector("#ifoodLink")
+var linkedinLink = document.querySelector("#linkedinLink")
+var mapsLink = document.querySelector("#mapsLink")
+var telefoneLink = document.querySelector("#telefoneLink") 
